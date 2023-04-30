0b8f8702137a14537a5383edca6d063baa38d17b
tsushumi <94306145+tsushumi@users.noreply.github.com>
2023-04-30 14:28:26 +0300

    Add files via upload

---
+++ question1/DemoForm.vb
@@ -0,0 +1,8 @@
+﻿Public Class DemoForm
+    Private Sub DemoForm()
+        Me.Size = New System.Drawing.Size(My.Computer.Screen.WorkingArea.Width * 0.75, My.Computer.Screen.WorkingArea.Height * 0.5)
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Call DemoForm()
+    End Sub
+End Class
---
+++ question2/Multiplication Table.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+
+    Private Sub MultiplicationTable()
+        RichTextBox1.AppendText("Multiplication Table")
+        RichTextBox1.AppendText(ControlChars.Lf)
+        RichTextBox1.AppendText("* 1 2 3 4 5 6 7")
+        RichTextBox1.AppendText(ControlChars.Lf)
+        For i As Integer = 1 To 7
+            RichTextBox1.AppendText(i & " ")
+            For j As Integer = 1 To 7
+                RichTextBox1.AppendText(i * j & " ")
+            Next
+            RichTextBox1.AppendText(ControlChars.Lf)
+        Next
+        RichTextBox1.AppendText(ControlChars.Lf)
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Call MultiplicationTable()
+    End Sub
+End Class
